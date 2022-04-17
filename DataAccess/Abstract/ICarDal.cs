72283441c7cfffd0fbf53f11a38cf98449e8ceea
egemenkerimgolu <egemen@elkomyazilim.com>
2022-04-17 23:13:18 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // Car Tablosnunun Veri erişim katmanı (Data access layer)
+    // Ekle sil güncelle getir gibi operasyonlar
+    public interface ICarDal
+    {
+        // GetById, GetAll, Add, Update, Delete
+        List<Car> GetAll();
+        void Add(Car car);
+        void Update(Car car);  
+        void Delete(Car car);
+        void GetById(int id);
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    // Bellekte çalışacak şekilde kodlayalım
+    public class InMemoryCarDal : ICarDal
+    {
+        // Veri varmış gibi davranacağımız için
+        List<Car> _cars;
+        // Proje başlatılınca bellekte araba listesi oluştur
+        public InMemoryCarDal()
+        {// Uygulama çaşıştığı anda
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,DailyPrice=500,ModelYear="2021",Description="Spor"},
+                new Car{CarId=2,BrandId=1,ColorId=2,DailyPrice=590,ModelYear="2012",Description="Spor"},
+                new Car{CarId=3,BrandId=2,ColorId=1,DailyPrice=700,ModelYear="2019",Description="Aile Arabası"},
+                new Car{CarId=4,BrandId=2,ColorId=3,DailyPrice=490,ModelYear="2008",Description="Binek"},
+                new Car{CarId=5,BrandId=2,ColorId=1,DailyPrice=750,ModelYear="2022",Description="Sedan"},
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            // LINQ
+            Car carToDelete = _cars.SingleOrDefault(c=>c.CarId==car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        // Burayı Düzenle ??
+        public void GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c=>c.CarId==car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
